7f806fd70b83b9a257f073dd56f5000bb54da417
hasitha <hasithamadusanka90@gmail.com>
2019-07-21 14:56:28 +0530

    dome

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -1,71 +1,27 @@
 import { Component, OnInit, OnDestroy, Input, Output, EventEmitter } from '@angular/core';
+import { TimerService } from './timer.service';
 
 @Component({
   selector: 'app-timer',
   templateUrl: './timer.component.html',
-  styleUrls: ['./timer.component.scss']
+  styleUrls: ['./timer.component.scss'],
+  providers: [TimerService]
 })
 export class TimerComponent implements OnInit, OnDestroy {
-
+  
   @Output() onComplete = new EventEmitter<void>();
   @Input() init:number = 20;
 
-  private countdownTimerRef:any = null;
-  public countdown:number = 0;
-  public paused:boolean = true;
 
-  constructor() { }
+
+  constructor(public timerService: TimerService ) { }
 
   ngOnInit(): void {
-    // this.startCountdown();
-    if(this.init && this.init > 0){
-      this.countdown = this.init;
-    }
+     this.timerService.restartCountdown(this.init);
   }
 
-  ngOnDestroy():void{
-    this.clearTimeout();
-  }
-
-  restartCountdown(){
-    if(this.init && this.init > 0) {
-      this.paused = true;
-      this.clearTimeout();
-      this.countdown = this.init;
-    }
-  }
-
-  toogleCoutdown(){
-    this.paused = !this.paused;
-    if(this.paused === false) {
-      this.doCountdown();
-    }else{
-      this.clearTimeout();
-    }
-  }
-
-  private doCountdown(){
-    this.countdownTimerRef = setTimeout(()=>{
-      this.countdown = this.countdown -1;
-      this.processCountdown();
-    }, 1000);
-  }
-
-  private processCountdown(){
-    if(this.countdown == 0){
-      this.onComplete.emit();
-      console.log("--countdown end--");
-    }
-    else{
-      this.doCountdown();
-    }
-  }
-
-  private clearTimeout(){
-    if(this.countdownTimerRef){
-      clearTimeout(this.countdownTimerRef);
-      this.countdownTimerRef = null;
-    }
+  ngOnDestroy(): void {
+    this.timerService.destroy();
   }
 
 }
