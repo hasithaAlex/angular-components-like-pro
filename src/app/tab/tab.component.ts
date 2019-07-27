1c72a0bea060ff95f198c7ee6f400b2a97a896d3
hasitha <hasithamadusanka90@gmail.com>
2019-07-27 09:52:53 +0530

    chages

--- src/app/tab/tab.component.ts
+++ src/app/tab/tab.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Tab } from "./tab.interface";
+import { TabsComponent } from 'app/tabs/tabs.component';
 
 @Component({
   selector: 'app-tab',
@@ -8,12 +9,13 @@ import { Tab } from "./tab.interface";
 })
 export class TabComponent implements OnInit, Tab {
 
-  @Input() title:string;
-  public isActive:boolean = false;
+  @Input() title: string;
+  public isActive: boolean = false;
 
-  constructor() { }
+  constructor(public tabs: TabsComponent) { }
 
   ngOnInit() {
+    this.tabs.addTab(this);
   }
 
 }
