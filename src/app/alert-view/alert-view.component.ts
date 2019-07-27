46a29d8dfc93b4915fd542a7a09c3ac04e34fd5a
hasitha <hasithamadusanka90@gmail.com>
2019-07-27 09:02:04 +0530

    frinsh

--- src/app/alert-view/alert-view.component.ts
+++ src/app/alert-view/alert-view.component.ts
@@ -6,11 +6,7 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./alert-view.component.scss']
 })
 export class AlertViewComponent implements OnInit {
-
-  @Output() onCancel = new EventEmitter<void>();
-  @Output() onSubmit = new EventEmitter<number>();
-
-
+ 
   constructor() { }
 
   ngOnInit() {
